d54809df41a9e6a0c6b043b91d08178e5daafec5
James Spargo <jspargo@thoughtworks.com>
2017-01-07 15:54:04 -0600

    JS - right align the amounts printed out by the ExpensePrinter;

--- src/main/java/org/spargonaut/io/ExpensePrinter.java
+++ src/main/java/org/spargonaut/io/ExpensePrinter.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ExpensePrinter {
     public static void printExpensesAsHumanReadable(List<Expense> expenses) {
         for (Expense expense : expenses) {
-            System.out.format("%-15s %-30s %-10s\n",
+            System.out.format("%-15s %-30s %10s\n",
                     expense.getTimestamp().toLocalDate(),
                     expense.getMerchant(),
                     expense.getAmount());
