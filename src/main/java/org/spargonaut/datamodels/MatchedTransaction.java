886d51bdc1a4b880bb84ce6ec268ceb6ca2a34bf
James Spargo <jspargo@thoughtworks.com>
2017-02-18 17:22:02 -0600

    JS - remove code that is no longer used due to the implementation of the generic UnusedCollector

--- src/main/java/org/spargonaut/datamodels/MatchedTransaction.java
+++ src/main/java/org/spargonaut/datamodels/MatchedTransaction.java
@@ -9,14 +9,6 @@ public class MatchedTransaction {
     private CreditCardActivity matchedCreditCardActivity;
     private Expense matchedExpense;
 
-    public boolean containsExpense(Expense expenseToCompare) {
-        return matchedExpense.equals(expenseToCompare);
-    }
-
-    public boolean containsCreditCardActivity(CreditCardActivity creditCardActivityToCompare) {
-        return matchedCreditCardActivity.equals(creditCardActivityToCompare);
-    }
-
     public boolean contains(Transaction transactionToCompare) {
         if (transactionToCompare.getClass().equals(CreditCardActivity.class)) {
             return matchedCreditCardActivity.equals(transactionToCompare);
