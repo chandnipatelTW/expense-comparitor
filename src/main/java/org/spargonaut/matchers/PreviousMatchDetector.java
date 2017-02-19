854c3692c00cf17ac73f6a6cb6f9ca032698b2c5
James Spargo <jspargo@thoughtworks.com>
2017-02-18 19:44:00 -0600

    JS - refactor the logic in the PreviousMatchDetector

--- src/main/java/org/spargonaut/matchers/PreviousMatchDetector.java
+++ src/main/java/org/spargonaut/matchers/PreviousMatchDetector.java
@@ -7,10 +7,11 @@ import java.util.List;
 
 public class PreviousMatchDetector {
     public boolean isPreviouslyMatched(Transaction transaction, List<MatchedTransaction> matchedTransactionList) {
-        boolean isMatched = false;
         for (MatchedTransaction matchedTransaction : matchedTransactionList) {
-            isMatched = matchedTransaction.contains(transaction) || isMatched;
+            if (matchedTransaction.contains(transaction)) {
+                return true;
+            };
         }
-        return isMatched;
+        return false;
     }
 }
