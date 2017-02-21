bc88be410fbe42ccd3aff0df651bc021590ffbcd
James Spargo <jspargo@thoughtworks.com>
2017-02-20 20:37:41 -0600

    JS - refactor out a for loop and an if/else block in favor of a filter;

--- src/main/java/org/spargonaut/io/printer/ChargePrinter.java
+++ src/main/java/org/spargonaut/io/printer/ChargePrinter.java
@@ -3,51 +3,46 @@ package org.spargonaut.io.printer;
 import org.spargonaut.datamodels.ActivityType;
 import org.spargonaut.datamodels.CreditCardActivity;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ChargePrinter {
 
-    // FIXME - separate out the payments from the charges
+    // FIXME - separate out the Fees from the charges
 
     public static void printChargesAsHumanReadable(Set<CreditCardActivity> activities) {
-        Set<CreditCardActivity> payments = new HashSet<>();
-        Set<CreditCardActivity> charges = new HashSet<>();
+        Set<CreditCardActivity> payments = activities.stream()
+                .filter(activity -> activity.getType() == ActivityType.PAYMENT)
+                .collect(Collectors.toSet());
 
-        separateChargesAndPayments(activities, payments, charges);
+        Set<CreditCardActivity> charges = activities.stream()
+                .filter(activity -> {
+                    ActivityType activityType = activity.getType();
+                    return (activityType == ActivityType.SALE || activityType == ActivityType.FEE);
+                })
+                .collect(Collectors.toSet());
 
-        String chargesString = "charges";
-        printActivities(charges, chargesString);
+        printHeaderString("charges", charges.size());
+        printActivities(charges);
 
         System.out.println();
 
-        String paymentString = "payments";
-        printActivities(payments, paymentString);
+        printHeaderString("payments", payments.size());
+        printActivities(payments);
     }
 
-    private static void separateChargesAndPayments(Set<CreditCardActivity> activities, Set<CreditCardActivity> payments, Set<CreditCardActivity> charges) {
-        for (CreditCardActivity activity : activities) {
-            if (activity.getType() == ActivityType.PAYMENT) {
-                payments.add(activity);
-            } else {
-                charges.add(activity);
-            }
-        }
+    private static void printActivities(Set<CreditCardActivity> charges) {
+        charges.forEach(charge -> {
+            System.out.format("%-10s %-15s %-15s %-30s %10s\n",
+                    charge.getType().getValue(),
+                    charge.getTransactionDate().toLocalDate(),
+                    charge.getPostDate().toLocalDate(),
+                    charge.getDescription(),
+                    charge.getAmount());
+        });
     }
 
-    private static void printActivities(Set<CreditCardActivity> charges, String chargesString) {
-        System.out.format(chargesString + " (%d) -----------------------------------------------------------------------\n", charges.size());
-        for (CreditCardActivity charge : charges) {
-            printActivity(charge);
-        }
-    }
-
-    private static void printActivity(CreditCardActivity activity) {
-        System.out.format("%-10s %-15s %-15s %-30s %10s\n",
-                activity.getType().getValue(),
-                activity.getTransactionDate().toLocalDate(),
-                activity.getPostDate().toLocalDate(),
-                activity.getDescription(),
-                activity.getAmount());
+    private static void printHeaderString(String headerType, int size) {
+        System.out.format(headerType + " (%d) -----------------------------------------------------------------------\n", size);
     }
 }
