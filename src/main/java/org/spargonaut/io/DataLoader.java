67dfaa5144f878adae3fb2c6b6179bf31fdd80b8
James Spargo <jspargo@thoughtworks.com>
2017-02-20 23:13:29 -0600

    JS - refactor out another for loop in favor of a foreach and a filter

--- src/main/java/org/spargonaut/io/DataLoader.java
+++ src/main/java/org/spargonaut/io/DataLoader.java
@@ -2,7 +2,6 @@ package org.spargonaut.io;
 
 import org.spargonaut.io.parser.Parser;
 
-import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -28,26 +27,25 @@ public class DataLoader<T> {
     }
 
     public void load(String directoryName, Parser<T> parser) {
-        csvFileLoader.getFileNamesIn(directoryName).forEach(csvFile -> {
+        csvFileLoader.getFileNamesIn(directoryName)
+        .forEach(csvFile -> {
             Set<T> uniqueParsedThings = parser.parseFile(csvFile)
                     .stream()
                     .filter(parsedThing -> !things.contains(parsedThing))
                     .collect(Collectors.toSet());
-                    things.addAll(uniqueParsedThings);
-                });
+            things.addAll(uniqueParsedThings);
+        });
     }
 
-    public void ignore(String directoryNameOfFilesToIgnore, Parser parser) {
-        Set<File> csvFileNames = csvFileLoader.getFileNamesIn(directoryNameOfFilesToIgnore);
-
-        for (File csvFile : csvFileNames) {
-            Set<T> parsedThings = parser.parseFile(csvFile);
-            for (T parsedThing : parsedThings) {
-                ignoredThings.add(parsedThing);
-                if (things.contains(parsedThing)) {
-                    things.remove(parsedThing);
-                }
-            }
-        }
+    public void ignore(String directoryNameOfFilesToIgnore, Parser<T> parser) {
+        csvFileLoader.getFileNamesIn(directoryNameOfFilesToIgnore)
+        .forEach(csvFile -> {
+            Set<T> uniqueParsedThings = parser.parseFile(csvFile)
+                    .stream()
+                    .filter(things::contains)
+                    .collect(Collectors.toSet());
+            things.removeAll(uniqueParsedThings);
+            ignoredThings.addAll(uniqueParsedThings);
+        });
     }
 }
