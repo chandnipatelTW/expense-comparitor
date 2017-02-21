119cbb3eb2011a0c827a2c0c65f2aaf73d294302
James Spargo <jspargo@thoughtworks.com>
2017-02-20 20:54:21 -0600

    JS - refactored out a for loop from the matchedTransactionPrinter

--- src/main/java/org/spargonaut/io/printer/MatchedTransactionPrinter.java
+++ src/main/java/org/spargonaut/io/printer/MatchedTransactionPrinter.java
@@ -9,21 +9,17 @@ import java.util.Set;
 public class MatchedTransactionPrinter {
     public static void printMatchedTransactions (Set<MatchedTransaction> matchedTransactions) {
         System.out.format(" transactions (%d) ------------------------------------------------------------------\n", matchedTransactions.size());
-        for (MatchedTransaction matchedTransaction : matchedTransactions) {
-            printMatchedTransaction(matchedTransaction.getMatchedCreditCardActivity(),
-                    matchedTransaction.getMatchedExpense());
-        }
-    }
-
-    private static void printMatchedTransaction (CreditCardActivity creditCardActivity, Expense expense) {
-        System.out.format("               %-50s %-15s %-15s %10s\nmatched with:  %-50s %-31s %10s\n\n",
-                creditCardActivity.getDescription(),
-                creditCardActivity.getTransactionDate().toLocalDate(),
-                creditCardActivity.getPostDate().toLocalDate(),
-                creditCardActivity.getAmount(),
-                expense.getMerchant(),
-                expense.getTimestamp().toLocalDate(),
-                expense.getAmount());
-
+        matchedTransactions.forEach( matchedTransaction -> {
+            CreditCardActivity creditCardActivity = matchedTransaction.getMatchedCreditCardActivity();
+            Expense expense = matchedTransaction.getMatchedExpense();
+            System.out.format("               %-50s %-15s %-15s %10s\nmatched with:  %-50s %-31s %10s\n\n",
+                    creditCardActivity.getDescription(),
+                    creditCardActivity.getTransactionDate().toLocalDate(),
+                    creditCardActivity.getPostDate().toLocalDate(),
+                    creditCardActivity.getAmount(),
+                    expense.getMerchant(),
+                    expense.getTimestamp().toLocalDate(),
+                    expense.getAmount());
+        });
     }
 }
