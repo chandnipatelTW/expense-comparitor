e658fbd4076157c53fd8b8983eb041adb29332cb
James Spargo <jspargo@thoughtworks.com>
2017-02-20 20:05:27 -0600

    JS - eradication of the usage of List<>

--- src/main/java/org/spargonaut/matchers/UnmatchedCollector.java
+++ src/main/java/org/spargonaut/matchers/UnmatchedCollector.java
@@ -4,7 +4,6 @@ import org.spargonaut.datamodels.MatchedTransaction;
 import org.spargonaut.datamodels.Transaction;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,10 +11,10 @@ public class UnmatchedCollector<T extends Transaction> {
 
     public Set<T> collect(Set<T> items, Collection<Set<MatchedTransaction>> matchedTransactionLists) {
         Class itemClassType = items.iterator().next().getClass();
-        List<Transaction> matchedItems = matchedTransactionLists.stream()
+        Set<Transaction> matchedItems = matchedTransactionLists.stream()
                 .flatMap(Set::stream)
                 .map( matchedTransaction -> matchedTransaction.getItemOfType(itemClassType) )
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
 
         return items.stream()
                 .filter( item -> !matchedItems.contains(item) )
