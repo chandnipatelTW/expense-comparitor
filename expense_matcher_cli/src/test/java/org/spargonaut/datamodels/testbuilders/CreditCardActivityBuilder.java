0f4cb0da543832f2c2725784a76de2b88ad69620
James Spargo <jspargo@thoughtworks.com>
2017-03-01 12:29:06 -0600

    JS - move the printer classes to the CLI project since the library project doesn't use them

---
+++ expense_matcher_cli/src/test/java/org/spargonaut/datamodels/testbuilders/CreditCardActivityBuilder.java
@@ -0,0 +1,55 @@
+package org.spargonaut.datamodels.testbuilders;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.joda.time.DateTime;
+import org.spargonaut.datamodels.ActivityType;
+import org.spargonaut.datamodels.CreditCardActivity;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class CreditCardActivityBuilder {
+
+    private Random randomizer = new Random();
+
+    private ActivityType type = ActivityType.SALE;
+    private DateTime transactionDate = DateTime.now();
+    private DateTime postDate = DateTime.now().minusDays(randomizer.nextInt());
+    private String description = RandomStringUtils.random(20);
+    private BigDecimal amount = new BigDecimal(randomizer.nextDouble());
+
+    public CreditCardActivity build() {
+        return new CreditCardActivity(type, transactionDate, postDate, description, amount);
+    }
+
+    public CreditCardActivityBuilder setType(ActivityType type) {
+        this.type = type;
+        return this;
+    }
+
+    public CreditCardActivityBuilder setTransactionDate(DateTime transactionDate) {
+        this.transactionDate = transactionDate;
+        return this;
+    }
+
+    public CreditCardActivityBuilder setPostDate(DateTime postDate) {
+        this.postDate = postDate;
+        return this;
+    }
+
+    public CreditCardActivityBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public CreditCardActivityBuilder setAmount(double amount) {
+        BigDecimal formattedBigDecimal = new BigDecimal(amount);
+        this.amount = formattedBigDecimal.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        return this;
+    }
+
+    public CreditCardActivityBuilder setBigDecimalAmount(BigDecimal amount) {
+        this.amount = amount;
+        return this;
+    }
+}
---
+++ expense_matcher_cli/src/test/java/org/spargonaut/datamodels/testbuilders/ExpenseBuilder.java
@@ -0,0 +1,90 @@
+package org.spargonaut.datamodels.testbuilders;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.joda.time.DateTime;
+import org.spargonaut.datamodels.Expense;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class ExpenseBuilder {
+
+    private Random randomizer = new Random();
+
+    private DateTime timestamp = new DateTime(2016, 12, 25, 0, 0);
+    private String merchant = RandomStringUtils.random(10);
+    private BigDecimal amount = new BigDecimal(randomizer.nextDouble());
+    private String mcc = randomizer.nextInt() + "";
+    private String category = RandomStringUtils.random(15);
+    private String tag = RandomStringUtils.random(15);
+    private String comment = RandomStringUtils.random(15);
+    private boolean reimbursable = randomizer.nextBoolean();
+    private String originalCurrency = RandomStringUtils.random(3);
+    private BigDecimal originalAmount = new BigDecimal(randomizer.nextDouble());
+    private String receiptURL = RandomStringUtils.random(50);
+
+    public Expense build() {
+        return new Expense(timestamp, merchant, amount, mcc, category, tag, comment, reimbursable, originalCurrency, originalAmount, receiptURL);
+    }
+
+    public ExpenseBuilder setTimestamp(DateTime timestamp) {
+        this.timestamp = timestamp;
+        return this;
+    }
+
+    public ExpenseBuilder setMerchant(String merchant) {
+        this.merchant = merchant;
+        return this;
+    }
+
+    public ExpenseBuilder setAmount(double amount) {
+        BigDecimal formattedBigDecimal = new BigDecimal(amount);
+        this.amount = formattedBigDecimal.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+        return this;
+    }
+
+    public ExpenseBuilder setBigDecimalAmount(BigDecimal amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public ExpenseBuilder setMcc(String mcc) {
+        this.mcc = mcc;
+        return this;
+    }
+
+    public ExpenseBuilder setCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public ExpenseBuilder setTag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    public ExpenseBuilder setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public ExpenseBuilder setReimbursable(boolean reimbursable) {
+        this.reimbursable = reimbursable;
+        return this;
+    }
+
+    public ExpenseBuilder setOriginalCurrency(String originalCurrency) {
+        this.originalCurrency = originalCurrency;
+        return this;
+    }
+
+    public ExpenseBuilder setOriginalAmount(double originalAmmount) {
+        this.originalAmount = new BigDecimal(originalAmmount);
+        return this;
+    }
+
+    public ExpenseBuilder setReceiptURL(String receiptUrl) {
+        this.receiptURL = receiptUrl;
+        return this;
+    }
+}
