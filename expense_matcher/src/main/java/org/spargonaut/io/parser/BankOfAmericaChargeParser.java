67f3646078b177de5bae8a76eb8cce3075a9e2b8
James Spargo <jspargo@thoughtworks.com>
2017-03-03 22:54:06 -0600

    JS - and initial implementation of a parser for Bank of America

---
+++ expense_matcher/src/main/java/org/spargonaut/io/parser/BankOfAmericaChargeParser.java
@@ -0,0 +1,56 @@
+package org.spargonaut.io.parser;
+
+import org.joda.time.DateTime;
+import org.spargonaut.datamodels.ActivityType;
+import org.spargonaut.datamodels.CreditCardActivity;
+import org.spargonaut.io.CSVFileReader;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BankOfAmericaChargeParser {
+    private final CSVFileReader csvFileReader;
+
+    public BankOfAmericaChargeParser(CSVFileReader csvFileReader) {
+        this.csvFileReader = csvFileReader;
+    }
+
+    public Set<CreditCardActivity> parseFile(File mockFile) {
+
+        Set<CreditCardActivity> creditCardActivities = new HashSet<>();
+
+        Set<String> chargeLines = csvFileReader.readCsvFile(mockFile);
+        for (String chargeLine : chargeLines) {
+            ActivityType defaultActivityType = ActivityType.SALE;
+            DateTime defaultTransactionDate = new DateTime(1999, 12, 23, 0, 0, 0);
+
+
+            String[] chargeTokens = chargeLine.split(",");
+
+            String postDateString = chargeTokens[0];
+            String[] postDateTokens = postDateString.split("/");
+            int postMonth = Integer.parseInt(postDateTokens[0]);
+            int postDay = Integer.parseInt(postDateTokens[1]);
+            int postYear = Integer.parseInt(postDateTokens[2]);
+            DateTime postDate = new DateTime(postYear, postMonth, postDay, 0, 0, 0);
+
+            String desctription = chargeTokens[2].replace("\"", "");
+
+
+            BigDecimal amount = new BigDecimal(chargeTokens[4]);
+            amount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            CreditCardActivity creditCardActivity = new CreditCardActivity(
+                    defaultActivityType,
+                    defaultTransactionDate,
+                    postDate,
+                    desctription,
+                    amount);
+            creditCardActivities.add(creditCardActivity);
+        }
+
+        return creditCardActivities;
+    }
+}
---
+++ expense_matcher/src/test/java/org/spargonaut/io/parser/BankOfAmericaChargeParserTest.java
@@ -0,0 +1,51 @@
+package org.spargonaut.io.parser;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.spargonaut.datamodels.ActivityType;
+import org.spargonaut.datamodels.CreditCardActivity;
+import org.spargonaut.datamodels.testbuilders.CreditCardActivityBuilder;
+import org.spargonaut.io.CSVFileReader;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BankOfAmericaChargeParserTest {
+
+    @Test
+    public void shouldParseBankOfAmericaChargeLineIntoACreditCardActivity() {
+        String chargeLine = "02/07/2017,24492157037637005325047,\"DOORDASH-MCALISTERS DE 650-681-9470 CA\",\"650-681-9470  CA \",-75.99";
+
+        File mockFile = mock(File.class);
+        CSVFileReader mockCSVFileReader = mock(CSVFileReader.class);
+        Set<String> chargeStrings = new HashSet<>(Arrays.asList(chargeLine));
+        when(mockCSVFileReader.readCsvFile(mockFile)).thenReturn(chargeStrings);
+
+        BankOfAmericaChargeParser bankOfAmericaChargeParser = new BankOfAmericaChargeParser(mockCSVFileReader);
+
+        DateTime expectedPostDate = new DateTime(2017, 02, 07, 0, 0, 0);
+        DateTime expectedTransactionDate = new DateTime(1999, 12, 23, 0, 0, 0);
+        String expectedDescription = "DOORDASH-MCALISTERS DE 650-681-9470 CA";
+        BigDecimal expectedAmount = new BigDecimal(-75.99);
+        expectedAmount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+        CreditCardActivity expectedCreditCardActivity = new CreditCardActivityBuilder()
+                .setType(ActivityType.SALE)
+                .setAmount(expectedAmount.doubleValue())
+                .setDescription(expectedDescription)
+                .setPostDate(expectedPostDate)
+                .setTransactionDate(expectedTransactionDate)
+                .build();
+
+        Set<CreditCardActivity> creditCardActivitySet = bankOfAmericaChargeParser.parseFile(mockFile);
+        assertThat(creditCardActivitySet.contains(expectedCreditCardActivity), is(true));
+    }
+}
