c2199704fb69023a6be5c826d6acd37a8bbd2419
spargonaut <spargonaut@gmail.com>
2017-06-20 19:43:31 -0700

    JS - refactor the BankOfAmericaChargeParser to use stream processing;

--- expense_matcher/src/main/java/org/spargonaut/io/parser/BankOfAmericaChargeParser.java
+++ expense_matcher/src/main/java/org/spargonaut/io/parser/BankOfAmericaChargeParser.java
@@ -8,8 +8,9 @@ import org.spargonaut.io.CSVFileReader;
 
 import java.io.File;
 import java.math.BigDecimal;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BankOfAmericaChargeParser implements Parser<CreditCardActivity> {
     private final CSVFileReader csvFileReader;
@@ -18,44 +19,49 @@ public class BankOfAmericaChargeParser implements Parser<CreditCardActivity> {
         this.csvFileReader = csvFileReader;
     }
 
-    public Set<CreditCardActivity> parseFile(File mockFile) {
+    public Set<CreditCardActivity> parseFile(File chargeFile) {
+        String chargeDelimiter = ",";
 
-        Set<CreditCardActivity> creditCardActivities = new HashSet<>();
+        return csvFileReader.readCsvFile(chargeFile).stream()
+                .filter(this::isParsable)
+                .map(chargeLine -> parseCreditCardActivity(chargeDelimiter, chargeLine))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+    }
 
-        Set<String> chargeLines = csvFileReader.readCsvFile(mockFile);
-        for (String chargeLine : chargeLines) {
-            if (!StringUtils.isBlank(chargeLine) &&
-                    !(isHeaderLine(chargeLine) || isCommentLine(chargeLine))) {
-                ActivityType defaultActivityType = ActivityType.SALE;
-                DateTime defaultTransactionDate = new DateTime(1999, 12, 23, 0, 0, 0);
-
-
-                String[] chargeTokens = chargeLine.split(",");
-
-                String postDateString = chargeTokens[0];
-                String[] postDateTokens = postDateString.split("/");
-                int postMonth = Integer.parseInt(postDateTokens[0]);
-                int postDay = Integer.parseInt(postDateTokens[1]);
-                int postYear = Integer.parseInt(postDateTokens[2]);
-                DateTime postDate = new DateTime(postYear, postMonth, postDay, 0, 0, 0);
-
-                String desctription = chargeTokens[2].replace("\"", "");
+    private CreditCardActivity parseCreditCardActivity(String chargeDelimiter, String chargeLine) {
+        CreditCardActivity creditCardActivity = null;
+        if (!StringUtils.isBlank(chargeLine)) {
+            ActivityType defaultActivityType = ActivityType.SALE;
+            DateTime defaultTransactionDate = new DateTime(1999, 12, 23, 0, 0, 0);
 
 
-                BigDecimal amount = new BigDecimal(chargeTokens[4]);
-                amount = amount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            String[] chargeTokens = chargeLine.split(chargeDelimiter);
 
-                CreditCardActivity creditCardActivity = new CreditCardActivity(
-                        defaultActivityType,
-                        defaultTransactionDate,
-                        postDate,
-                        desctription,
-                        amount);
-                creditCardActivities.add(creditCardActivity);
-            }
+            String postDateString = chargeTokens[0];
+            String[] postDateTokens = postDateString.split("/");
+            int postMonth = Integer.parseInt(postDateTokens[0]);
+            int postDay = Integer.parseInt(postDateTokens[1]);
+            int postYear = Integer.parseInt(postDateTokens[2]);
+            DateTime postDate = new DateTime(postYear, postMonth, postDay, 0, 0, 0);
+
+            String desctription = chargeTokens[2].replace("\"", "");
+
+            BigDecimal amount = new BigDecimal(chargeTokens[4]);
+            amount = amount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            creditCardActivity = new CreditCardActivity(
+                    defaultActivityType,
+                    defaultTransactionDate,
+                    postDate,
+                    desctription,
+                    amount);
         }
+        return creditCardActivity;
+    }
 
-        return creditCardActivities;
+    private boolean isParsable(String chargeLine) {
+        return !(isHeaderLine(chargeLine) || isCommentLine(chargeLine));
     }
 
     private boolean isCommentLine(String chargeLine) {
