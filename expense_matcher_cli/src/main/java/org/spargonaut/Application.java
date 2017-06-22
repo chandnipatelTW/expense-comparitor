3debe2b4f254a1221b00eeb2dc04f730839367cb
spargonaut <spargonaut@gmail.com>
2017-06-22 15:30:18 -0700

    JS - refactor out some duplication in the Application class

--- expense_matcher_cli/src/main/java/org/spargonaut/Application.java
+++ expense_matcher_cli/src/main/java/org/spargonaut/Application.java
@@ -6,10 +6,7 @@ import org.spargonaut.datamodels.MatchedTransaction;
 import org.spargonaut.io.CSVFileLoader;
 import org.spargonaut.io.CSVFileReader;
 import org.spargonaut.io.DataLoader;
-import org.spargonaut.io.parser.BankOfAmericaChargeParser;
-import org.spargonaut.io.parser.ExpenseParser;
-import org.spargonaut.io.parser.JPMCChargeParser;
-import org.spargonaut.io.parser.ParsableDirectory;
+import org.spargonaut.io.parser.*;
 import org.spargonaut.io.printer.ReportPrinter;
 import org.spargonaut.matchers.CloseDateMatcher;
 import org.spargonaut.matchers.ExactMatcher;
@@ -38,26 +35,19 @@ public class Application {
         Set<CreditCardActivity> creditCardActivities = new HashSet<>();
         Set<CreditCardActivity> ignoredCreditCardActivities = new HashSet<>();
 
-        String boaDirectorPath = createChargeFileDirectoryPath() + "bank_of_america";
-        ParsableDirectory<CreditCardActivity> boaDirectory = new ParsableDirectory<>(boaDirectorPath, new BankOfAmericaChargeParser(csvFileReader));
-        DataLoader<CreditCardActivity> boaDataLoader = new DataLoader<>(new CSVFileLoader(), boaDirectory);
+        DataLoader<CreditCardActivity> boaDataLoader = createChargeDataLoader("bank_of_america", new BankOfAmericaChargeParser(csvFileReader));
         creditCardActivities.addAll(boaDataLoader.loadTransactions());
         ignoredCreditCardActivities.addAll(boaDataLoader.getIgnoredData());
 
-        String jpmcDirectoryPath = createChargeFileDirectoryPath() + "jpmc";
-        ParsableDirectory<CreditCardActivity> jpmcDirectory = new ParsableDirectory<>(jpmcDirectoryPath, new JPMCChargeParser(csvFileReader));
-        DataLoader<CreditCardActivity> jpmcDataLoader = new DataLoader<>(new CSVFileLoader(), jpmcDirectory);
+        DataLoader<CreditCardActivity> jpmcDataLoader = createChargeDataLoader("jpmc", new JPMCChargeParser(csvFileReader));
         creditCardActivities.addAll(jpmcDataLoader.loadTransactions());
         ignoredCreditCardActivities.addAll(jpmcDataLoader.getIgnoredData());
 
-        String expenseDirectoryName = expenseFileDirectoryPath() + "expensify";
-        ParsableDirectory<Expense> expensifyExpensesDirectory = new ParsableDirectory<>(expenseDirectoryName, new ExpenseParser(csvFileReader));
-        DataLoader<Expense> expenseDataLoader = new DataLoader<>(new CSVFileLoader(), expensifyExpensesDirectory);
+        DataLoader<Expense> expenseDataLoader = createExpenseDataLoader("expensify", new ExpenseParser(csvFileReader));
         Set<Expense> expenses = expenseDataLoader.loadTransactions();
 
         TransactionProcessor transactionProcessor = new TransactionProcessor(creditCardActivities, expenses);
-        Set<TransactionMatcher> matchers = aSetOfMatchers(new ExactMatcher(), new CloseDateMatcher(), new FuzzyMerchantExactAmountMatcher());
-        transactionProcessor.processTransactions(matchers);
+        transactionProcessor.processTransactions(aSetOfMatchers(new ExactMatcher(), new CloseDateMatcher(), new FuzzyMerchantExactAmountMatcher()));
 
         Map<String, Set<MatchedTransaction>> matchedTransactionsMap = transactionProcessor.getMatchedTransactions();
         Set<CreditCardActivity> unmatchedCreditCardActivity = transactionProcessor.getUnmatchedCreditCardActivies();
@@ -71,15 +61,25 @@ public class Application {
                                     unmatchedExpenses);
     }
 
+    private DataLoader<Expense> createExpenseDataLoader(String directoryName, ExpenseParser expenseParser) {
+        ParsableDirectory<Expense> expensifyExpensesDirectory = new ParsableDirectory<>(fullPathToExpenseDirectory(directoryName), expenseParser);
+        return new DataLoader<>(new CSVFileLoader(), expensifyExpensesDirectory);
+    }
+
+    private DataLoader<CreditCardActivity> createChargeDataLoader(String directoryName, Parser<CreditCardActivity> parser) {
+        ParsableDirectory<CreditCardActivity> parsableDirectory = new ParsableDirectory<>(fullPathToChargeDirectory(directoryName), parser);
+        return new DataLoader<>(new CSVFileLoader(), parsableDirectory);
+    }
+
     private HashSet<TransactionMatcher> aSetOfMatchers(TransactionMatcher... transactionMatchers) {
         return new HashSet<>(Arrays.asList(transactionMatchers));
     }
 
-    private String expenseFileDirectoryPath() {
-        return dataDirectoryName + fileSeparator + expenseFileDirectoryName + fileSeparator;
+    private String fullPathToExpenseDirectory(String directory) {
+        return dataDirectoryName + fileSeparator + expenseFileDirectoryName + fileSeparator + directory;
     }
 
-    private String createChargeFileDirectoryPath() {
-        return dataDirectoryName + fileSeparator + chargeFileDirectoryName + fileSeparator;
+    private String fullPathToChargeDirectory(String directory) {
+        return dataDirectoryName + fileSeparator + chargeFileDirectoryName + fileSeparator + directory;
     }
 }
